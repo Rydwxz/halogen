f18934a80b5619260e71e12cd23fafcdf02a254f
Redward <collardmusic@gmail.com>
2024-12-18 16:43:52 -0800

    iced2

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,40 @@
+use iced::{
+    alignment,
+    widget::{container, text},
+    Element, Length, Sandbox, Settings,
+};
 
+fn main() -> iced::Result {
+    HApp::run(Settings::default())
+}
 
+struct HApp {
+    // project: bool,
+}
+
+#[derive(Debug)]
+enum HMessage {
+    Session(SessionMessage),
+}
+
+#[derive(Debug)]
+enum SessionMessage {
+    OpenFileBrowser,
+    LoadFile,
+}
+
+impl HApp {
+    fn new() -> Self {
+        Self {}
+    }
+
+    fn title(&self) -> String {
+        String::from("Halogen")
+    }
+
+    fn update(&mut self, _message: Self::Message) {}
+
+    fn view(&self) -> iced::Element<Self::Message> {
+        "Hello Halogen".into()
+    }
+}
